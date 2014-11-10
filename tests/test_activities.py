5be3765a3dad884275137091959519af60883905
Noam Rubin <rubin.m.noam@gmail.com>
2014-11-09 22:11:21 -0800

    fixed the assertions so they check the right thing

--- tests/test_activities.py
+++ tests/test_activities.py
@@ -2,6 +2,10 @@
 
 import unittest
 import app
+import datetime
+
+import json
+
 class ActivityTestCase(unittest.TestCase):
 
 	def setUp(self):
@@ -9,8 +13,23 @@ class ActivityTestCase(unittest.TestCase):
 
 	def test_add_activity(self):
 		rv = self.app.post('/api/activities/add',data=dict(name='DummyActivity',description='A Dummy Activity'),follow_redirects=True)
-		assert "DummyActivity" in rv
+		assert "DummyActivity" in rv.data
+
+	def test_add_big_activity(self):
+		name = "A Big Activity"
+		description = "A large activity with a large description, mostly describing useless things but just in case we will make it super large. Hello world, lorem ipsum dolor sit amet, yes I did just write that from memory. You know what? <strong> Lets </strong> write some <p> html too! </p>"
+		duration = 60 * 60 * 1000 #AKA an hour
+		difficulty = "EASY"
+		pricing = json.dumps({"1 lesson":55,"5 lessons":300})
+		photos = ["http://i.imgur.com/43ddDwa.jpg","http://i.imgur.com/43ddDwa.jpg"]
+		schedule = [datetime.datetime.now(),datetime.datetime.now()]
+
+		bigActivity = dict(name=name,description=description,duration=duration,difficulty=difficulty,pricing=pricing,photos=photos,schedule=schedule)
+
+		rv = self.app.post('/api/activities/add',data=bigActivity)
+		assert "Big Activity" in rv.data
 
 	def test_get_activity(self):
 		rv = self.app.get('/api/activities/get',data=dict(name="DummyActivity"))
-		assert "DummyActivity" in rv
+		assert "DummyActivity" in rv.data
+
